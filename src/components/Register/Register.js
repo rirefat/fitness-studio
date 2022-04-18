67fd382b87a9916616e02aee5f3af7c8880a78c1
rirefat <rirefat.official@gmail.com>
2022-04-18 17:19:58 +0600

    Done Register Page design

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import './Register.css'
 
 const Register = () => {
-    return (
-        <div>
-            <h1 className='register-title'>Register Now</h1>
+    const navigate = useNavigate();
+
+    const navigateLogin =()=>{
+        navigate('/login');
+    }
+    const handleSubmit = event =>{
+        event.preventDefault();
+    }
+    return (        
+        <div>            
+            <div className='container w-25 mx-auto login-form'>
+            <h1 className='register-title mb-3'>Register Now</h1>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" placeholder="Write your name" required/>
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required/>
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="warning" type="submit">
+                    Register
+                </Button>
+                </Form>
+                <p className='my-3'>Already Have An Account? <span className='highlighted-text' onClick={navigateLogin}>Login Now</span></p>
+        </div>
         </div>
     );
 };
