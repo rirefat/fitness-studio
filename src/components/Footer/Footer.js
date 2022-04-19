eb4f30a92f2356a9ff7b17c3cdf891c322f208d0
rirefat <rirefat.official@gmail.com>
2022-04-19 22:12:41 +0600

    Added Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Footer.css'
+import img from '../../images/logo.png'
 
 const Footer = () => {
     let currentYear = new Date().getFullYear();
     return (
-        <footer>
+        <footer className='text-center'>
+            <img src={img} className="img-fluid mt-3" alt="" />
+            <p>King Street, Melbourne</p>
+            <p>Victoria 3000 Australia</p>
+            <p>8 800 595 82 12</p>
+            <p>info@fitness-studio.com</p>
             <h4>Copyright 2020-{currentYear} || All Right Reserved.</h4>
+            
         </footer>
     );
 };
